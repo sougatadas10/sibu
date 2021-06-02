a491cc9c6c4533134694856cbc155150dfa83c8b
sougatadas10 <sougatadas10@users.noreply.github.com>
2021-06-02 22:13:50 +0530

    Update shibkali.groovy

--- shibkali.groovy
+++ shibkali.groovy
@@ -1,13 +1,11 @@
 node('win-build') {
     checkout scm   
-    
-    stage('clone code') { //This stage should be avoided if build script resides on same repo as project code
-        git clone git@bitbucket.org:sougatadas10/merger.git //make sure ssh keys are added in jenkins
-    }
+    currentBuild.result = 'SUCCESS'
 
     stage('build') {
-        def msbuild =  "path/to/msbuild/MsBuild.exe"
-        def exitStatus = bat(returnStatus: true, script: "${msbuild} solution.sln /p:Configuration=Debug")
+        def msbuild =  "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\msbuild.exe"
+        def slnfile = "TRA e-Services Manager.sln"
+        def exitStatus = bat(returnStatus: true, script: "${msbuild} ${slnfile} /p:Configuration=Debug")
         if (exitStatus != 0){
             currentBuild.result = 'FAILURE'
             error 'build failed'
@@ -15,17 +13,14 @@ node('win-build') {
     }
 
     stage('upload artifacts') {
-        //Install Jenkins Artifactory Plug-in
-        def server = Artifactory.newServer url: 'artifactory-url', username: 'username', password: 'password'
-        def uploadSpec = """{
-            "files": [
-                {
-                    "pattern": "bazinga/*froggy*.zip",
-                    "target": "bazinga-repo/froggy-files/"
-                }
-            ]
-        }"""
-        server.upload spec: uploadSpec
+        if (currentBuild.result = 'SUCCESS') {
+            sh """
+            git add --all
+            git commit -m \"Check in the build artifacts\"
+            git push 
+            """
+        }
+
     }
 
 
